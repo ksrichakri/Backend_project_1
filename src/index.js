5b583a34b78e5889be335f45e6c8e3c2d147f45c
ksrichakri <142304892+ksrichakri@users.noreply.github.com>
2025-06-09 01:53:24 +0530

    Router and controller basic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 
 dotenv.config({
     path:'./env'
